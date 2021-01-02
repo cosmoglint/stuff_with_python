14be2cac786bb37211f6106c8fa58211443bdbf1
cosmoglint <ramprakashsmail@gmail.com>
2021-01-02 20:16:45 +0530

    testing  naiive mutation logic

--- typing_monkey.py
+++ typing_monkey.py
@@ -6,8 +6,8 @@ import random
 target = "METHINKS IT IS LIKE A WEASEL"
 
 
-tester = "METHINKS IT IS LIKE A WEASEL"
-# tester = "YETHINKSPITXISHLIKEFA WQYSEY"
+# tester = "METHINKS IT IS LIKE A WEASEL"
+tester = "YETHINKSPITXISHLIKEFA WQYSEY"
 
 # these will be the sample sets for the monkey
 alphabets_low = string.ascii_lowercase + " "
@@ -21,18 +21,33 @@ min_survival = len(target) * variables
 samples_per_generation = 10
 
 
+
 def selection(generation_list):
     pass
-def mutate(current,prob_list):
-    pass
+def mutate(current,current_variation_list):
+    global alphabets_up
+    new = ""
+    for i,val in enumerate(current_variation_list):
+        if not(val):
+            new += current[i]
+        else:
+            new += random.choice(alphabets_up)
+    return new
+    # pass
 
 def survival_score(current,target):
     score = 0
+    current_variation_list = []
     for i,val in enumerate(current):
-        score += abs(ord(val) - ord(target[i]))
-    return score
+        cur_score = abs(ord(val) - ord(target[i]))
+        current_variation_list.append(cur_score)
+        score += cur_score
+    return score, current_variation_list
 
 
 
-print(survival_score(tester,target))
-print(alphabets_up,alphabets_low)
+
+t_score, t_current_variation_list = survival_score(tester,target)
+# print(t_score,t_current_variation_list)
+print(mutate(tester,t_current_variation_list))
+# print(alphabets_up,alphabets_low)
