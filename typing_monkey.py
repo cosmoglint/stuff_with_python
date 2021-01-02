1a8e804bd196fa6481bf321c0beb8583f66f316f
cosmoglint <ramprakashsmail@gmail.com>
2021-01-02 18:43:53 +0530

    ADD:  starting string and target

---
+++ typing_monkey.py
@@ -0,0 +1,10 @@
+import string
+import random
+
+
+# this is the string to be reached
+target = "METHINKS IT IS LIKE A WEASEL"
+
+# these will be the sample sets for the monkey
+alphabets_low = string.ascii_lowercase
+alphabets_up = string.ascii_uppercase
