3211a9b084a820ee7cdb8e8e9e84699dd8f3656b
cosmoglint <ramprakashsmail@gmail.com>
2021-01-02 18:56:10 +0530

    ADD:  survival score value checker

--- typing_monkey.py
+++ typing_monkey.py
@@ -4,7 +4,21 @@ import random
 
 # this is the string to be reached
 target = "METHINKS IT IS LIKE A WEASEL"
+tester = "METHINKS IT IS LIKE A WEASEL"
+
+# tester = "YETHINKSPITXISHLIKEFA WQYSEY"
 
 # these will be the sample sets for the monkey
-alphabets_low = string.ascii_lowercase
-alphabets_up = string.ascii_uppercase
+alphabets_low = string.ascii_lowercase + " "
+alphabets_up = string.ascii_uppercase + " "
+
+def survival_score(current,target):
+    score = 0
+    for i,val in enumerate(current):
+        score += abs(ord(val) - ord(target[i]))
+    return score
+
+print(survival_score(tester,target))
+
+
+print(alphabets_up,alphabets_low)
